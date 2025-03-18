b38f76a00556a1eb701c5c559111b6d46d8ba9a2
k99k5 <k99k5@qq.com>
2025-03-18 14:15:41 +0800

    perf(Busuanzi): optimize script loading and display
    - Move script loading to onActivated and onMounted lifecycle hooks
    - Add initial values to counters to improve user experience
    - Remove unnecessary display:none styles

--- app/components/Busuanzi.vue
+++ app/components/Busuanzi.vue
@@ -24,19 +24,20 @@ function loadScript() {
     });
 }
 
-onMounted(() => loadScript());
+onActivated(() => setTimeout(loadScript,500))
+onMounted(() => setTimeout(loadScript,500))
 watch(() => route.fullPath, () => loadScript(), {immediate: true})
 </script>
 
 <template>
 	<div v-if="appConfig.libs.bsz.enable" class="text-sm text-gray-500 flex gap-3">
-		<div id="busuanzi_container_site_pv" style="display:none">
+		<div id="busuanzi_container_site_pv">
 			{{ t('访问量') }}
-			<span id="busuanzi_value_site_pv"></span>
+			<span id="busuanzi_value_site_pv">0</span>
 		</div>
-		<div id="busuanzi_container_site_uv" style="display:none">
+		<div id="busuanzi_container_site_uv">
 			{{ t('访客数') }}
-			<span id="busuanzi_value_site_uv"></span>
+			<span id="busuanzi_value_site_uv">0</span>
 		</div>
 	</div>
 </template>
