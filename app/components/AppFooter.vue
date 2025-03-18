cccf9454b4ef760ffcdc5a5f45e348c80ca272f4
k99k5 <k99k5@qq.com>
2025-03-18 11:54:18 +0800

    feat(components): add Busuanzi component and update AppFooter
    - Create new Busuanzi component for site statistics
    - Update AppFooter to include Busuanzi and copyright information
    - Add script for loading Busuanzi script and handling route changes

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts">
-
+<script lang="ts" setup>
+const appConfig = useAppConfig();
 </script>
 
 <template>
-	<div />
+	<div class="flex flex-col justify-center items-center">
+		<Busuanzi />
+		<div class="text-center">
+			<p class="text-sm text-gray-500">
+				&copy; {{ new Date().getFullYear() }} {{ appConfig.siteName }}
+			</p>
+		</div>
+	</div>
 </template>
---
+++ app/components/Busuanzi.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const appConfig = useAppConfig();
+const route = useRoute();
+const {t} = useI18n();
+
+function loadScript() {
+    console.log(1)
+    if (document.getElementById('busuanzi')) {
+        document.getElementById('busuanzi').remove();
+    }
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.id = 'busuanzi';
+        script.src = appConfig.libs.bsz.src;
+        script.onload = () => {
+            resolve(true);
+        };
+        script.onerror = () => {
+            reject();
+        };
+        document.body.appendChild(script);
+    });
+}
+
+onMounted(() => loadScript());
+watch(() => route.fullPath, () => loadScript(), {immediate: true})
+</script>
+
+<template>
+	<div v-if="appConfig.libs.bsz.enable" class="text-sm text-gray-500">
+		<div id="busuanzi_container_site_pv" style="display:none">
+			{{ t('访问量') }}
+			<span id="busuanzi_value_site_pv"></span>
+		</div>
+		<div id="busuanzi_container_site_uv" style="display:none">
+			{{ t('访客数') }}
+			<span id="busuanzi_value_site_uv"></span>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
