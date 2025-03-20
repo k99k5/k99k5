427a77718e0265f2823ca26b9ddec82fdce854c0
k99k5 <k99k5@qq.com>
2025-03-20 11:46:49 +0800

    refactor(error): optimize error page layout and SEO
    - Remove unused v-tooltip directive from h1 element
    - Improve line break handling in p element for better readability
    - Update SEO meta descriptions to use newline character instead of HTML break

--- app/components/AppError.vue
+++ app/components/AppError.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="h-full flex-1 mx-auto px-4 text-center flex flex-col justify-center items-center">
-        <h1 v-tooltip.bottom="props?.message" class="text-4xl font-bold mb-6">{{ t('Oops，页面不见了') }}</h1>
-        <p v-html="t('🤔 你可以看看热门文章<br />或者搜索其他内容？')" class="mb-8 text-gray-600"></p>
+        <h1 class="text-4xl font-bold mb-6">{{ t('Oops，页面不见了') }}</h1>
+        <p v-tooltip.bottom="props?.message" v-html="t('🤔 你可以看看热门文章\n或者搜索其他内容？').replace(/\n/g, '<br>')" class="mb-8 text-gray-600"></p>
         <Button @click="emit('handleError')"
             class="px-6 py-3 bg-primary text-white rounded-lg hover:bg-primary-dark transition-colors">
             {{ t('返回首页') }}
